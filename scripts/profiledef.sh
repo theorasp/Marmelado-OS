30bb36df0387c39bdadbe4f47e1e08b5e8d7e6ff
theorasp <theo+github@netsquad.de>
2026-03-19 20:11:38 +0100

    Fix bootmodes x64 only

--- scripts/profiledef.sh
+++ scripts/profiledef.sh
@@ -6,9 +6,10 @@ iso_application="Marmelado OS"
 iso_version="1.0"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-b' '1M')
 file_permissions=()
+EOF
