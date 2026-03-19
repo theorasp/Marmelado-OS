13b559164c23b266fff4eb315620327f5fde9fff
theorasp <theo+github@netsquad.de>
2026-03-19 20:24:47 +0100

    Fix bootmodes and add grub

--- scripts/profiledef.sh
+++ scripts/profiledef.sh
@@ -2,14 +2,12 @@
 iso_name="marmelado-os"
 iso_label="MARMELADO"
 iso_publisher="Marmelado OS"
-iso_application="Marmelado OS"
 iso_version="1.0"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux' 'uefi.grub')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-b' '1M')
 file_permissions=()
-EOF
